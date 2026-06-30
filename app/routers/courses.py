0e88adba7ea2644f23bf9019b0b13a622a64ffd3
amlf amlf <jyj20030816@gmail.com>
2026-06-30 14:58:03 +0900

    refactor: separate couse service logic

--- app/routers/courses.py
+++ app/routers/courses.py
@@ -3,6 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, HTTPException, Query, status
 
 from app.schemas.course import CourseCreate, CourseResponse
+from app.services import course_service
 
 
 router = APIRouter(
@@ -11,28 +12,13 @@ router = APIRouter(
 )
 
 
-# DB 구현 전까지 사용할 임시 메모리 저장소
-courses: list[dict] = []
-next_course_id = 1
-
-
 @router.post(
     "",
     response_model=CourseResponse,
     status_code=status.HTTP_201_CREATED,
 )
 def create_course(course: CourseCreate):
-    global next_course_id
-
-    new_course = {
-        "id": next_course_id,
-        **course.model_dump(),
-    }
-
-    courses.append(new_course)
-    next_course_id += 1
-
-    return new_course
+    return course_service.create_course(course)
 
 
 @router.get(
@@ -53,22 +39,11 @@ def get_courses(
         Query(ge=1, le=100),
     ] = 10,
 ):
-    filtered_courses = courses
-
-    if q is not None:
-        keyword = q.lower()
-
-        filtered_courses = [
-            course
-            for course in courses
-            if keyword in course["name"].lower()
-            or (
-                course["description"] is not None
-                and keyword in course["description"].lower()
-            )
-        ]
-
-    return filtered_courses[skip : skip + limit]
+    return course_service.get_courses(
+        q=q,
+        skip=skip,
+        limit=limit,
+    )
 
 
 @router.get(
@@ -76,11 +51,12 @@ def get_courses(
     response_model=CourseResponse,
 )
 def get_course(course_id: int):
-    for course in courses:
-        if course["id"] == course_id:
-            return course
+    course = course_service.get_course_by_id(course_id)
 
-    raise HTTPException(
-        status_code=status.HTTP_404_NOT_FOUND,
-        detail="과목을 찾을 수 없습니다.",
-    )
+    if course is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="과목을 찾을 수 없습니다.",
+        )
+
+    return course
